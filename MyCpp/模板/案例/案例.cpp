c0d23281523687cc7882a0c7bc779cdb714faccd
2078825250 <2078825250@qq.com>
2020-12-23 11:08:02 +0800

    20201223-11:07

--- MyCpp/模板/案例/案例.cpp
+++ MyCpp/模板/案例/案例.cpp
@@ -6,6 +6,9 @@ using namespace std;
 //算法 选择
 //测试 char 数组、int数组
 
+
+template<typename T>
+void myswap(T& a, T& b);
 //排序算法
 template<typename T>  // typename可以用class代替
 void sort(T arr[], int len) {
@@ -14,37 +17,60 @@ void sort(T arr[], int len) {
 		int max = i;  //设定最大值下标
 		for (int j = i + 1; j < len; j++)
 		{
-			if (arr[max] < arr[j])
+			if (arr[max] > arr[j])
 			{
 				max = j;
 			}
 		}
 		if (max != i)
-		{
+		{		
 			
 			myswap(arr[max], arr[i]);
 		}
 	}
 }
 
-
-void test01() {
-	//测试
-	char charArray[] = "hfsha";
-	int num = sizeof(charArray) / sizeof(char);
-	myswap()
-}
-
 //交换函数模板
 template<typename T>
-void myswap(T& a, T& b)
+void myswap(T &a, T&b)
 {
 	T temp = a;
 	a = b;
 	b = temp;
 }
 
+//打印数组模板
+template<typename T>
+void printfArray(T arr[], int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		cout << arr[i]  ;
+	}
+	cout << endl;
+}
+
+
+void test01() {
+	//测试char数组
+	char charArray[] = "hfsha";
+	int num = sizeof(charArray) / sizeof(char);  //计算数组的长度
+	//sizeof(char)是一个整型表达式,计算出运算对象在计算机的内存中所占用的字节数量，该表达式的返回值是一个整数，
+	sort(charArray, num-1); //num-1去掉"\0"
+	printfArray(charArray,num);
+}
+
 void test02()
+{
+	int numArray[] = { 4,2,5,7,8,1 };
+	int num = sizeof(numArray) / sizeof(int);
+	sort(numArray, num);
+	printfArray(numArray, num-1);
+}
+
+
+
+void test03()
 {
 	int a = 10;
 	int b = 20;
@@ -58,9 +84,9 @@ void test02()
 
 int main() {
 
-
 	test02();
 
 	system("pause");
 	return 0;
 }
+
