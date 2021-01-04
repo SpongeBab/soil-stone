9e43a2f8d459747bcf0138515012aaff9f3a8054
2078825250 <2078825250@qq.com>
2021-01-04 17:34:50 +0800

    恢复上一版本

--- MyCpp/常用算法/for-each/for-each.cpp
+++ MyCpp/常用算法/for-each/for-each.cpp
@@ -1,17 +1,33 @@
 ﻿#include<iostream>
 using namespace std;
 #include<vector>
+#include<algorithm>
+
+void printf1(int val)
+{
+	cout << val << " ";
+}
+
+
 
 void test()
 {
 	vector<int> v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+
+	}
+
+	for_each(v.begin(), v.end(), printf1);
+	
 }
 
 
 
 int main()
 {
-	
+	test();
 
 
 	system("pause");
